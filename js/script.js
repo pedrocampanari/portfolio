5b515e17a4fb3b09a3d23b3ce443a8dfed2b95c0
Pedro Campanari <pedrocampanari09@hotmail.com>
2024-09-05 21:25:46 -0300

    next

--- js/script.js
+++ js/script.js
@@ -4,15 +4,22 @@ var tag = document.getElementById("text2");
 var html = document.getElementById("text2").innerHTML;
 var attr = tag.setAttribute("data", html);
 var txt = tag.getAttribute("data");
-var speed = 400;
+var speed = 300;
 
 
 function typeWriter() {
     if (i <= txt.length) {
         document.getElementById("text2").innerHTML = txt.slice(0, i + 1);
         i++;
+        document.getElementById("text2").style.color = `rgb(115, ${7*i}, ${17*i})`
+        
+        console.log(`rgb(115, ${7*i}, ${17*i})`)
         setTimeout(typeWriter, speed);
+    }else {
+        document.getElementById("text2").style.animation = 'textEffect 3s infinite';
     }
+
+   
 }
 
 typeWriter();
